b7e91dee78fe9e7e98a0c5b912baacd1cec6224a
Aline Sestren Lopes <alinesestren@gmail.com>
2024-09-10 19:58:05 +0200

    Criação de programa para cálculo de fatorial

---
+++ README.md
@@ -0,0 +1,4 @@
+# Exemplo Código Disciplina Gerência de Configuração
+
+Este é um exemplo de código para a disciplina de Gerência de Configuração.
+
---
+++ script.js
@@ -0,0 +1,43 @@
+
+function fatorar() {
+    // Obter o valor do input
+    const numero = parseInt(document.getElementById('id_num').value);
+
+    // Verificar se o valor é válido
+     if (isNaN(numero) || numero < 0) {
+        alert("Por favor, insira um número válido.");
+        return;
+    }
+
+    // Calcular o fatorial e montar a string de exibição
+    let resultado = 1;
+    let expressao = "";
+    
+    for (let i = numero; i > 0; i--) {
+        resultado *= i;
+        expressao += i;
+        if (i > 1) {
+            expressao += " X "; // Adiciona " X " entre os números
+        }
+    }
+
+    // Criar a section dinamicamente
+    let section = document.getElementById('res');
+
+    // Se já existir uma section com id=res, remove para criar uma nova
+    if (section) {
+        section.remove();
+    }
+
+    // Cria uma nova section com id=res
+    section = document.createElement("section");
+    section.id = "res";
+
+    // Adiciona o texto de resultado dentro da section
+    section.innerHTML = `<h1>O fatorial de ${numero} é igual a ${resultado}</h1>
+    <p> O cálculo fatorial é feito da seguinte forma: 
+    <h3>${expressao} = ${resultado}</h3>`;
+
+    // Insere a section no body
+    document.body.appendChild(section);
+}
